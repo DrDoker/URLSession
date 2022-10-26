cef3f6b2fb6eb2c1f6e79f44aee9d2e9f28f0979
Serhii Tkachenko <doker.tk@gmail.com>
2022-10-26 23:06:42 +0200

    Initial Commit

---
+++ URLSession HW.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+import CryptoKit
+
+// MARK: - Задание
+
+let currenciesURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+
+func getData(urlReuaest: String) {
+    let urlReuaest = URL(string: urlReuaest)
+    guard let url = urlReuaest else { return }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if error != nil {
+            let errorString = error?.localizedDescription ?? ""
+            print("Error: \(errorString)")
+        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            guard let data = data else { return }
+            guard let dataAsString = String(data: data, encoding: .utf8) else { return }
+            print("Server response code: \(response.statusCode)")
+            print("Data from the server: \(dataAsString)")
+        } else if let response = response as? HTTPURLResponse {
+            print("Server response code: \(response.statusCode)")
+        }
+    }.resume()
+}
+
+getData(urlReuaest: currenciesURL)
+
+// MARK: - Задание⭐
+func md5Hash(_ source: String) -> String {
+    return Insecure.MD5.hash(data: source.data(using: .utf8)!).map { String(format: "%02hhx", $0) }.joined()
+}
+
+let startURL = "https://gateway.marvel.com/v1/public/"
+let marvelPrivateKey = "2895055c5371280d7387073cc3ef477e45632869"
+let marvelPublicKey = "d8182c561967ebc637775965e3484849"
+
+let ts = "Serhii-Tkachenko"
+let hashString = ts + marvelPrivateKey + marvelPublicKey
+let hashMD5 = md5Hash(hashString)
+
+let marvelURL = "\(startURL)characters?name=Iron%20Man&ts=\(ts)&apikey=\(marvelPublicKey)&hash=\(hashMD5)"
+
+getData(urlReuaest: marvelURL)
